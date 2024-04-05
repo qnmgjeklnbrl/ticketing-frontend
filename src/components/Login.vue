4afc0dd00663c0308981dfb85c64e1a839fb3482
qnmgjeklnbrl <rns0721@naver.com>
2024-04-05 19:54:42 +0900

    qwe

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<!-- Login.vue -->
+<template>
+    <div>
+      <input v-model="username" placeholder="Username" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button @click="login">Login</button>
+    </div>
+</template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const username = ref('');
+  const password = ref('');
+  
+  const login = () => {
+    // 로그인 처리 로직을 여기에 작성하세요.
+    console.log('로그인 버튼이 클릭되었습니다.');
+  };
+  </script>
+  
